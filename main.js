5b7b6fffd61f86980ae3cc9d732421ce113926bc
gman311 <44368627+gman311@users.noreply.github.com>
2018-11-06 08:47:33 -0500

    html and css done

---
+++ main.js
@@ -0,0 +1,14 @@
+function FizzBuzz() {
+
+}
+for(var i % 5; === 0 && i % 3; === 0) {
+      console.log('FizzBuzz');
+  }
+    else if (i % 5 === 0) {
+    console.log('Buzz');
+  }
+  else if (i % 3 === 0) {
+    console.log('Fizz');
+
+ }
+}
